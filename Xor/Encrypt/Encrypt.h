d7f168756fff56949c5b6d3a6e988bb4d84061e6
falme <devfalme@163.com>
2018-12-11 17:19:06 +0700

    无话可说

---
+++ Xor/Encrypt/Encrypt.h
@@ -0,0 +1,17 @@
+//
+//  Encrypt.h
+//  Xor
+//
+//  Created by devfalme on 2018/12/11.
+//  Copyright © 2018 devfalme. All rights reserved.
+//
+
+#ifndef Encrypt_h
+#define Encrypt_h
+
+#import <Foundation/Foundation.h>
+
+NSString *decode(NSString *string);
+NSString *encode(NSString *string);
+
+#endif /* Encrypt_h */
---
+++ Xor/Encrypt/Encrypt.m
@@ -0,0 +1,33 @@
+//
+//  Encrypt.c
+//  Xor
+//
+//  Created by devfalme on 2018/12/11.
+//  Copyright © 2018 devfalme. All rights reserved.
+//
+
+#import "Encrypt.h"
+
+//因子
+static NSString const *factor = @"ef37c9111210854f5986fc9ebb5548b2ae";
+
+NSString *decode(NSString *string) {
+    return encode(string);
+}
+
+NSString *encode(NSString *string) {
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    NSInteger length = factor.length;
+    const char *keys = [factor cStringUsingEncoding:NSASCIIStringEncoding];
+    unsigned char cKey[length];
+    memcpy(cKey, keys, length);
+    NSMutableData *encryptData = [[NSMutableData alloc] initWithCapacity:length];
+    const Byte *point = data.bytes;
+    for (int i = 0; i < data.length; i++) {
+        int l = i % length;
+        char c = cKey[l];
+        Byte b = (Byte) ((point[i]) ^ c);
+        [encryptData appendBytes:&b length:1];
+    }
+    return [[NSString alloc] initWithData:encryptData.copy encoding:NSUTF8StringEncoding];
+}
